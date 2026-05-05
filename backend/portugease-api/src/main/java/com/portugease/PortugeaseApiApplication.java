b392db90a3aacbe3154e6e102f07bed7607f5c7e
Colin Regan <colinregan123@gmail.com>
2026-05-05 19:53:11 +0100

    Initial backend commit

---
+++ backend/portugease-api/src/main/java/com/portugease/PortugeaseApiApplication.java
@@ -0,0 +1,12 @@
+package com.portugease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortugeaseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortugeaseApiApplication.class, args);
+    }
+}
---
+++ backend/portugease-api/src/test/java/com/portugease/PortugeaseApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.portugease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortugeaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
