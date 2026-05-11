d4100e6a0ea5a15f564d84040736343260264750
Colin Regan <colinregan123@gmail.com>
2026-05-11 19:17:08 +0100

    Spring Boot backend foundation Enums Exceptions Entities Repositories

---
+++ backend/portugease-api/src/main/java/com/portugease/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.portugease.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ProblemDetail handleResourceNotFound(ResourceNotFoundException exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problemDetail.setTitle("Resource not found");
+        problemDetail.setDetail(exception.getMessage());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleValidation(MethodArgumentNotValidException exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle("Validation error");
+        problemDetail.setDetail("The request body contains invalid data.");
+        return problemDetail;
+    }
+}
